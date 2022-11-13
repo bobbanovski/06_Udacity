0e820e7582b41e1c674f8a03bc4c5b1e539b478f
bobbanovski@gmail.com <bobbanovski@gmail.com>
2022-11-13 16:53:26 +1100

    Added table insert, select and drop queries

--- DataEngineering/DataModelling/06 cassandra2.py
+++ DataEngineering/DataModelling/06 cassandra2.py
@@ -52,12 +52,76 @@ try:
 except Exception as e:
     print(e)
 
-query_insert_music_library = """"""
-query_insert_album_library = """"""
+query_insert_music_library = "INSERT INTO music_library (year, artist_name, album_name) "
+query_insert_music_library += "VALUES (%s, %s, %s)"
 
-query = "SELECT COUNT(*) FROM music_library"
+query_insert_album_library = "INSERT INTO album_library (artist_name, year, album_name) "
+query_insert_album_library += "VALUES (%s, %s, %s)"
+
+music_library_data = [
+    (1970, "The Beatles", "Let it Be"),
+    (1965, "The Beatles", "Rubber Soul"),
+    (1965, "The Who", "My Generation"),
+    (1966, "The Monkees", "The Monkees"),
+    (1970, "The Carpenters", "Close To You")
+]
+music_library_data_count = len(music_library_data)
+missing_data = 0
+for data in music_library_data:
+    try:
+        session.execute(query_insert_music_library, data)
+    except Exception as e:
+        print(e)
+        missing_data += 1
+
+print (str(music_library_data_count - missing_data) + " Entries entered, " + str(missing_data) + " Entries failed entry")
+
+album_library_data =[
+    ("The Beatles", 1970, "Let it Be"),
+    ("The Beatles", 1965, "Rubber Soul"),
+    ("The Who", 1965, "My Generation"),
+    ("The Monkees", 1966, "The Monkees"),
+    ("The Carpenters", 1970, "Close To You")
+]
+album_library_data_count = len(album_library_data)
+missing_data = 0
+for data in album_library_data:
+    try:
+        session.execute(query_insert_album_library, data)
+    except Exception as e:
+        print(e)
+        missing_data += 1
+print(str(album_library_data_count - missing_data) + " Entries entered, " + str(missing_data) + " Entries failed entry")
+
+query = "SELECT * FROM music_library WHERE YEAR = 1970"
 try:
-    count = session.execute(query)
-    print(count)
+    rows = session.execute(query)
 except Exception as e:
     print(e)
+
+for row in rows:
+    print(row.year, row.artist_name, row.album_name,)
+
+query = "SELECT * FROM album_library WHERE ARTIST_NAME = 'The Beatles'"
+try:
+    rows = session.execute(query)
+except Exception as e:
+    print(e)
+
+for row in rows:
+    print(row.artist_name, row.year,  row.album_name,)
+
+query_drop = "DROP TABLE music_library"
+try:
+    rows = session.execute(query_drop)
+except Exception as e:
+    print(e)
+
+query_drop = "DROP TABLE album_library"
+try:
+    rows = session.execute(query_drop)
+except Exception as e:
+    print(e)
+
+session.shutdown()
+cluster.shutdown()
